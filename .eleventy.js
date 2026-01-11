98d8ddfc0164d96540c835262b68a25a960528e8
Your Name <you@example.com>
2026-01-11 13:27:31 +0800

    Configure path prefix and url filters for GitHub Pages compatibility

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets/css");
   
   return {
+    pathPrefix: "/price-intel/",
     dir: {
       input: "src",
       output: "_site",
